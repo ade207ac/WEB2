8151554dc76b3b29d3774a9542b2291523c4fbad
muhammad luthfi s.f <muhammadadiel19@gmail.com>
2025-03-11 22:55:53 +0700

    Add files via upload

---
+++ admin/panggil.php
@@ -0,0 +1,20 @@
+<?php 
+    include_once 'top.php';
+?>
+<div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <?php 
+                    include_once 'menu.php';
+                ?>
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">hasil project</h1>
+                    <?php
+                        include_once 'Pertemuan12/index.html';
+                    ?>
+                </div>
+</div>
+<?php 
+    include_once 'bottom.php';
+?>
+
